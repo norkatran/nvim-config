2b0764b297f21a08fcc51bd29c4cd43ac42bb4db
Marnie Lysander <marnielysander@gmail.com>
2025-05-12 09:30:40 +0100

    Fix fuzzy finder to ignore certain directories

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 
+local excluded_dirs = { "node_modules", "vendor", "build", ".git" }
+
 local find_git_path = function()
   local root_dir
   for dir in vim.fs.parents(vim.api.nvim_buf_get_name(0)) do
@@ -9,7 +11,7 @@ local find_git_path = function()
     end
   end
   if root_dir then
-    return vim.fs.normalize(root_dir)
+    return root_dir
   else
     return vim.fs.dirname(vim.api.nvim_buf_get_name(0))
   end
@@ -35,27 +37,36 @@ end
 
 local fuzzy_finder = function(input)
   local root = find_git_path()
-  vim.notify(vim.inspect(root))
   local files = vim.fs.find(
     function (file, path)
+      local local_path = path:sub(root:len() + 2)
+      for _, value in pairs(excluded_dirs) do
+        local idx = local_path:find(value)
+        if idx == 1 then
+          return nil
+        end
+      end
       for token in string.gmatch(input, "[^%s]+") do
-        if string.find(string.lower(file), string.lower(token)) then
+        if string.find(file:lower(), token:lower()) then
           return true
         end
-        if string.find(string.lower(path), string.lower(token)) then
+        if string.find(local_path:lower(), token:lower()) then
           return true
         end
       end
     end,
     {
       type = 'file',
-      limit = math.huge,
+      limit = 200, --math.huge,
       path = root
     })
-    file_selector(files, nil, 2)
+    file_selector(files, root, 2)
 end
 
 -- normal mode
+-- Leader key is used to set mnemonic groups of functionality
+
+-- f group -> files
 vim.keymap.set('n', '<Leader>ff', function()
   vim.ui.input({ prompt = 'File: ' }, function (input)
     if input then
@@ -72,15 +83,20 @@ vim.keymap.set('n', '<Leader>ft', function()
 end)
 vim.keymap.set('n', '<Leader>fs', function()
   vim.ui.input({ prompt = 'Needle: ' }, function (input)
-    local raw = vim.fn.system({ "rg", "-l", input })
-    local files = {}
-    for file in string.gmatch(raw, "[^%s]+") do
-      table.insert(files, file)
+    if input then
+      local root = find_git_path()
+      local raw = vim.fn.system({ "rg", "-l", input, root })
+      local files = {}
+      for file in string.gmatch(raw, "[^%s]+") do
+        table.insert(files, file)
+      end
+      table.sort(files)
+      file_selector(files, root, 2)
     end
-    table.sort(files)
-    file_selector(files, nil, 1)
   end)
 end)
+
+-- p group -> projects
 
 -- insert mode
 
